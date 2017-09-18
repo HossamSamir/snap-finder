39c2d74e2cbdf4d2fa72c2b84f5a4e26a1c69c43
Hossam Samir <Hossameos1@gmail.com>
2017-09-18 05:15:55 +0200

    login

--- screens/Login.js
+++ screens/Login.js
@@ -8,7 +8,8 @@ import {
   TouchableOpacity,
   View,
   TextInput,
-  AsyncStorage
+  AsyncStorage,
+  KeyboardAvoidingView
 } from 'react-native';
 import { WebBrowser } from 'expo';
 
@@ -24,11 +25,31 @@ export default class Login extends React.Component {
     const { navigate } = this.props.navigation;
     return (
       <View style={styles.container}>
-        <Text style={{ fontSize: 40, fontWeight: 'bold', color: 'crimson' }}>Logo</Text>
-        <TouchableOpacity
-          style={{  width: '50%', margin: 30, backgroundColor: 'crimson', }}>
-          <Text style={{ color: 'white', padding: 20, borderRadius: 10, fontSize: 23, textAlign: 'center' }}>Login</Text>
-        </TouchableOpacity>
+          <Image source={require('../assets/images/bg.jpg')} style={{ width: '100%', height: '100%', position: 'absolute', resizeMode: 'cover' }}/>
+          <View style={{ flex: 1, backgroundColor: 'transparent', marginTop: '60%', marginVertical: '10%', width: '80%' }}>
+
+            <KeyboardAvoidingView behavior='position' style={{ flex: .5 }}>
+              <TextInput placeholder='username' placeholderTextColor='#6b7887' style={{ marginBottom: 30, borderBottomWidth: 1, borderBottomColor: '#6b7887', color: 'white' }} />
+              <TextInput placeholder='password' secureTextEntry={true} placeholderTextColor='#6b7887' style={{ marginBottom: 30, borderBottomWidth: 1, borderBottomColor: '#6b7887', color: 'white' }} />
+            </KeyboardAvoidingView>
+
+
+              <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+                <TouchableOpacity
+                  onPress={() => {
+                    this.props.navigation.navigate('Main', {})
+                  }}
+                  style={{
+                    borderWidth: 1,
+                    borderColor: 'white',
+                    borderRadius: 50,
+                    paddingHorizontal: 100,
+                    paddingVertical: 10,
+                   }}>
+                <Text style={{ color: 'white', fontSize: 17 }}>Login</Text>
+                </TouchableOpacity>
+              </View>
+          </View>
       </View>
     );
   }
@@ -37,8 +58,8 @@ export default class Login extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    width: '100%'
   },
 });
