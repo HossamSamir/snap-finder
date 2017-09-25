7b04728ac276931ea310e42c920fde08fe9a41e4
Hossam Samir <Hossameos1@gmail.com>
2017-09-25 22:23:06 +0200

    interests with icons

--- screens/InterestsScreen.js
+++ screens/InterestsScreen.js
@@ -7,17 +7,125 @@ import {
   Text,
   TouchableOpacity,
   View,
+  FlatList,
+  Dimensions,
 } from 'react-native';
-export default class InterestsScreen extends React.Component {
-  static navigationOptions = {
-    header: null
-  };
+import Carousel from 'react-native-looped-carousel';
+import { Ionicons } from '@expo/vector-icons';
+import { List, ListItem } from 'react-native-elements';
 
+const { width, height } = Dimensions.get('window');
+
+export default class InterestsScreen extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      users: [
+      [
+        {
+          name: '# sport',
+          avatar_url: 'https://scontent-cai1-1.xx.fbcdn.net/v/t34.0-12/22014707_535977623410806_1650694489_n.png?oh=bb585edb7ff77f85bb0b2997e7c2fa81&oe=59CB0253',
+          id: 1
+        },
+        {
+          name: '# art',
+          avatar_url: 'https://scontent-cai1-1.xx.fbcdn.net/v/t34.0-12/22014707_535977623410806_1650694489_n.png?oh=bb585edb7ff77f85bb0b2997e7c2fa81&oe=59CB0253',
+          id: 2
+        },
+      ],
+      [
+        {
+          name: '# sport',
+          avatar_url: 'https://scontent-cai1-1.xx.fbcdn.net/v/t34.0-12/22014707_535977623410806_1650694489_n.png?oh=bb585edb7ff77f85bb0b2997e7c2fa81&oe=59CB0253',
+          id: 3
+        },
+        {
+          name: '# art',
+          avatar_url: 'https://scontent-cai1-1.xx.fbcdn.net/v/t34.0-12/22014707_535977623410806_1650694489_n.png?oh=bb585edb7ff77f85bb0b2997e7c2fa81&oe=59CB0253',
+          id: 4
+        },
+      ],
+      [
+        {
+          name: '# sport',
+          avatar_url: 'https://scontent-cai1-1.xx.fbcdn.net/v/t34.0-12/22014707_535977623410806_1650694489_n.png?oh=bb585edb7ff77f85bb0b2997e7c2fa81&oe=59CB0253',
+          id: 5
+        },
+        {
+          name: '# art',
+          avatar_url: 'https://scontent-cai1-1.xx.fbcdn.net/v/t34.0-12/22014707_535977623410806_1650694489_n.png?oh=bb585edb7ff77f85bb0b2997e7c2fa81&oe=59CB0253',
+          id: 6
+        },
+      ],
+      [
+        {
+          name: '# sport',
+          avatar_url: 'https://scontent-cai1-1.xx.fbcdn.net/v/t34.0-12/22014707_535977623410806_1650694489_n.png?oh=bb585edb7ff77f85bb0b2997e7c2fa81&oe=59CB0253',
+          id: 1
+        },
+        {
+          name: '# art',
+          avatar_url: 'https://scontent-cai1-1.xx.fbcdn.net/v/t34.0-12/22014707_535977623410806_1650694489_n.png?oh=bb585edb7ff77f85bb0b2997e7c2fa81&oe=59CB0253',
+          id: 2
+        },
+      ],
+    ],
+      size: { width, height },
+      heartIcon: 'ios-heart-outline'
+    }
+  }
+  static navigationOptions = {
+    header: null,
+  };
+  _onLayoutDidChange = (e) => {
+    const layout = e.nativeEvent.layout;
+    this.setState({ size: { width: layout.width, height: layout.height } });
+  }
   render() {
     return (
-      <View style={{ flex: 1, backgroundColor: '#fff', alignItems: 'center', justifyContent: 'center' }}>
-        <Text style={{ fontSize: 40, fontWeight: 'bold', color: '#3498db' }}>Interests screen</Text>
-      </View>
-    )
+      <View style={styles.container}>
+        <Image
+          style={{ width, marginBottom: -38, resizeMode: 'contain'}}
+          source={require('../assets/images/header.jpg')}
+        />
+
+      <List containerStyle={{flex: 1, flexDirection: 'row', borderTopWidth: 0}}>
+            <FlatList
+              data = {this.state.users}
+              renderItem = {({ item }) => (
+                <View style={{ flex: 1, flexDirection: 'row',  backgroundColor: '#F6F7F8'}}>
+                  <TouchableOpacity style={{ flex: 1, }}>
+                    <Image
+                      style={{ flex: 1, height: 120, resizeMode: 'contain'}}
+                      source={{uri: item[0].avatar_url}}
+                    />
+                  <Text style={{ flex: 1, textAlign: 'center', fontSize: 19, padding: 20, fontWeight: 'bold', color: 'crimson' }}>{item[0].name}</Text>
+                  </TouchableOpacity>
+                  <TouchableOpacity style={{ flex: 1, margin: 10,  }}>
+                    <Image
+                      style={{ flex: 1, height: 120, resizeMode: 'contain'}}
+                      source={{uri: item[1].avatar_url}}
+                    />
+                  <Text style={{ flex: 1, textAlign: 'center', fontSize: 19, padding: 20, fontWeight: 'bold', color: 'crimson' }}>{item[1].name}</Text>
+                  </TouchableOpacity>
+                </View>
+              )}
+              keyExtractor={item => item[0].id}
+            />
+          </List>
+
+
+
+        </View>
+
+    );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+  },
+});
